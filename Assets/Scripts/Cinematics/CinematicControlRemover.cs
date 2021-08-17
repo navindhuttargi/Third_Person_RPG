d111c6f98c9f66c0afdf105d7bd393c32a7cfadc
navinsd <navind@tesseract.in>
2021-08-17 14:06:59 +0530

    Disabling the player control while intro cut scene

---
+++ Assets/Scripts/Cinematics/CinematicControlRemover.cs
@@ -0,0 +1,30 @@
+﻿using RPG.Control;
+using RPG.Core;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CinematicControlRemover : MonoBehaviour
+{
+    GameObject player;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        GetComponent<PlayableDirector>().played += onDisable;
+        GetComponent<PlayableDirector>().stopped += onEnable;
+    }
+
+    private void onEnable(PlayableDirector obj)
+    {
+        player.GetComponent<PlayerController>().enabled = true;
+    }
+
+    private void onDisable(PlayableDirector obj)
+    {
+        GameObject.FindGameObjectWithTag("Player");
+        player.GetComponent<ActionScheduler>().CancelCurrentAction();
+        player.GetComponent<PlayerController>().enabled = false;
+    }
+}
---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                GetComponent<PlayableDirector>().Play();
+                GetComponent<BoxCollider>().enabled = false;
+            }
+        }
+    }
+}
