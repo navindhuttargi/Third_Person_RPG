16e9e65c6926b46d1fa6b5449d7846ead9c71b66
navinsd <navind@tesseract.in>
2021-08-24 18:23:40 +0530

    Added fireball weapon, player experience and stats displaying player and enemy health

---
+++ Assets/Assets/Scripts/Resources/Health.cs
@@ -0,0 +1,75 @@
+using System;
+using RPG.Core;
+using RPG.Saving;
+using RPG.Stats;
+using UnityEngine;
+
+namespace RPG.Resources
+{
+    public class Health : MonoBehaviour, ISaveable
+    {
+        float healthPoints = -1f;
+
+        bool isDead = false;
+
+        private void Start()
+        {
+            if (healthPoints < 0)
+            {
+                healthPoints = GetComponent<BaseStats>().GetStat(Stat.Health);
+            }
+        }
+
+        public bool IsDead()
+        {
+            return isDead;
+        }
+
+        public void TakeDamage(GameObject instigator, float damage)
+        {
+            healthPoints = Mathf.Max(healthPoints - damage, 0);
+            if(healthPoints == 0)
+            {
+                Die();
+                AwardExperience(instigator);
+            }
+        }
+
+        public float GetPercentage()
+        {
+            return 100 * (healthPoints / GetComponent<BaseStats>().GetStat(Stat.Health));
+        }
+
+        private void Die()
+        {
+            if (isDead) return;
+
+            isDead = true;
+            GetComponent<Animator>().SetTrigger("die");
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
+        private void AwardExperience(GameObject instigator)
+        {
+            Experience experience = instigator.GetComponent<Experience>();
+            if (experience == null) return;
+
+            experience.GainExperience(GetComponent<BaseStats>().GetStat(Stat.ExperienceReward));
+        }
+
+        public object CaptureState()
+        {
+            return healthPoints;
+        }
+
+        public void RestoreState(object state)
+        {
+            healthPoints = (float) state;
+            
+            if (healthPoints <= 0)
+            {
+                Die();
+            }
+        }
+    }
+}
---
+++ Assets/Assets/Scripts/Stats/BaseStats.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Stats
+{
+    public class BaseStats : MonoBehaviour
+    {
+        [Range(1, 99)]
+        [SerializeField] int startingLevel = 1;
+        [SerializeField] CharacterClass characterClass;
+        [SerializeField] Progression progression = null;
+
+        int currentLevel = 0;
+
+        private void Start() 
+        {
+            currentLevel = CalculateLevel();
+            Experience experience = GetComponent<Experience>();
+            if (experience != null)
+            {
+                experience.onExperienceGained += UpdateLevel;
+            }
+        }
+
+        private void UpdateLevel() 
+        {
+            int newLevel = CalculateLevel();
+            if (newLevel > currentLevel)
+            {
+                currentLevel = newLevel;
+                print("Levelled Up!");
+            }
+        }
+
+        public float GetStat(Stat stat)
+        {
+            return progression.GetStat(stat, characterClass, GetLevel());
+        }
+
+        public int GetLevel()
+        {
+            if (currentLevel < 1)
+            {
+                currentLevel = CalculateLevel();
+            }
+            return currentLevel;
+        }
+
+        public int CalculateLevel()
+        {
+            Experience experience = GetComponent<Experience>();
+            if (experience == null) return startingLevel;
+
+            float currentXP = experience.GetPoints();
+            int penultimateLevel = progression.GetLevels(Stat.ExperienceToLevelUp, characterClass);
+            for (int level = 1; level <= penultimateLevel; level++)
+            {
+                float XPToLevelUp = progression.GetStat(Stat.ExperienceToLevelUp, characterClass, level);
+                if (XPToLevelUp > currentXP)
+                {
+                    return level;
+                }
+            }
+
+            return penultimateLevel + 1;
+        }
+    }
+}
